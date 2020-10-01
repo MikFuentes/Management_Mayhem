d305bd0a9838566d3885eafdfe9b9ed69aef64c3
MikFuentes <33886316+MikFuentes@users.noreply.github.com>
2020-10-01 19:58:06 +0800

    First Commit
    Created a new Project that handles basic multiplayer movement clientside.

---
+++ ManagementMayhem/Assets/Player.cs
@@ -0,0 +1,93 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] private Vector3 movement;
+    [SerializeField] private Vector3 jump = new Vector3();
+
+    [SerializeField] private float moveSpeed;
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+
+    [Client]
+    private void Update()
+    {
+        if (!hasAuthority) { return; }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //CmdJump();
+        }
+
+        //Controls
+        if (Input.GetKey(KeyCode.W))
+        {
+            verticalMove = moveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            verticalMove = -moveSpeed;
+        }
+        else
+        {
+            verticalMove = 0;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontalMove = -moveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            horizontalMove = moveSpeed;
+        }
+        else 
+        {
+            horizontalMove = 0;
+        }
+
+        //CmdMove();
+
+    }
+
+    [Client]
+
+    private void FixedUpdate()
+    {
+        movement = new Vector3(horizontalMove, verticalMove, 0f).normalized;
+        transform.position += movement * Time.fixedDeltaTime * moveSpeed;
+    }
+
+    //[Command]
+    //private void CmdMove()
+    //{
+    //    //Validate logic here
+
+    //    RpcMove();
+    //}
+
+    //[Command]
+    //private void CmdJump()
+    //{
+    //    //Validate logic here
+
+    //    RpcJump();
+    //}
+
+    //[ClientRpc]
+    //private void RpcMove() 
+    //{
+    //    //Debug.Log(horizontalMove + " ," + verticalMove + " , 0");
+    //    movement = new Vector3(horizontalMove, verticalMove, 0f).normalized;
+    //    transform.position += movement * Time.fixedDeltaTime * moveSpeed;
+    //}
+
+    //[ClientRpc]
+    //private void RpcJump()
+    //{
+    //    transform.Translate(jump);
+    //}
+}
---
+++ ManagementMayhem/Assets/PlayerCameraController.cs
@@ -0,0 +1,23 @@
+﻿using Cinemachine;
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : NetworkBehaviour
+{
+    [Header("Camera")]
+    [SerializeField] private Transform playerTransform = null;
+    [SerializeField] private CinemachineVirtualCamera virtualCamera = null;
+
+    private CinemachineTransposer transposer;
+
+    public override void OnStartAuthority()
+    {
+        transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+
+        virtualCamera.gameObject.SetActive(true);
+
+        enabled = true;
+    }
+}
